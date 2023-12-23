4d0eefb357690bcab5974efbd384979c6406997b
jayanth75 <150255250+jayanth75@users.noreply.github.com>
2023-12-23 12:22:36 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,12 +1,12 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://13.234.75.115:9000/',
+  serverUrl: 'http://13.234.186.175:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsMithunTechnologies',
-	    'sonar.login': 'squ_dd9c695804fe2d2f68846dc0fe99b20f7d906d88',
+	    'sonar.login': 'squ_d08426f4b6b844d7bf96b0446230909206780cf6',
 	    //'sonar.login': 'admin',
 	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
